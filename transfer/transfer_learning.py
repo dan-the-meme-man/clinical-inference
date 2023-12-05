86559fd34d8257565d2344c9bbe1163f2abff6fb
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-2.local>
2023-12-05 13:27:57 -0500

    clean code

--- transfer/transfer_learning.py
+++ transfer/transfer_learning.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from tqdm import tqdm
 import random
 import time
+import json
 from sklearn.utils import shuffle
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from transformers import AdamW, get_linear_schedule_with_warmup
@@ -14,6 +15,14 @@ from sklearn.metrics import classification_report
 tokenizer = AutoTokenizer.from_pretrained("microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext")
 model =BertModel.from_pretrained("microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext")
 
+MAX_LEN=512 # don't change
+SAVE_PATH = 'models/'
+EPOCH = 10
+HIDDEN_SIZE=100
+LEARNING_RATE = 5e-5
+BS = 16
+
+
 if torch.cuda.is_available():
     device = torch.device("cuda")
     print(f'There are {torch.cuda.device_count()} GPU(s) available.')
@@ -24,19 +33,13 @@ else:
     device = torch.device("cpu")
 
 
-MAX_LEN=512 # don't change
-SAVE_PATH = 'models/'
-EPOCH = 10
-HIDDEN_SIZE=100
-LEARNING_RATE = 5e-5
-
 
 def preprocess(text):
   return text
 
 label_num_pair = {"Entailment":0, "Contradiction":1}
 
-import json
+
 
 
 def data_preprocess(data):
@@ -117,18 +120,16 @@ dev_input_ids, dev_attention_masks = preprocessing_for_bert(dev_input)
 train_labels_ID = torch.tensor(train_labels)
 dev_labels_ID = torch.tensor(dev_labels)
 
-# For fine-tuning BERT, the authors recommend a batch size of 16 or 32.
-batch_size = 16
 
 # Create the DataLoader for our training set
 train_data = TensorDataset(train_input_ids, train_attention_masks, train_labels_ID)
 train_sampler = RandomSampler(train_data)
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=BS)
 
 # Create the DataLoader for our validation set
 dev_data = TensorDataset(dev_input_ids, dev_attention_masks, dev_labels_ID)
 dev_sampler = SequentialSampler(dev_data)
-dev_dataloader = DataLoader(dev_data, sampler=dev_sampler, batch_size=batch_size)
+dev_dataloader = DataLoader(dev_data, sampler=dev_sampler, batch_size=BS)
 
 
 class BertClassifier(nn.Module):
@@ -292,73 +293,6 @@ def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=Fal
 
     print("Training complete!")
 
-# def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=False):
-#   print("Start training...\n")
-#   for epoch_i in range(epochs):
-#   # =======================================
-#   #               Training
-#   # =======================================
-#   # Print the header of the result table
-#     print(f"{'Epoch':^7} | {'Batch':^7} | {'Train Loss':^12} | {'Val Loss':^10} | {'Val Acc':^9} | {'Elapsed':^9}")
-#     print("-"*70)
-#     # Measure the elapsed time of each epoch
-#     t0_epoch, t0_batch = time.time(), time.time()
-#     # Reset tracking variables at the beginning of each epoch
-#     total_loss, batch_loss, batch_counts = 0, 0, 0
-#     # Put the model into the training mode
-#     model.train()
-#     # For each batch of training data...
-#     for step, batch in enumerate(train_dataloader):
-#       batch_counts +=1
-#       # Load batch to GPU
-#       b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
-#       # Zero out any previously calculated gradients
-#       model.zero_grad()
-#       # Perform a forward pass. This will return logits.
-#       logits = model(b_input_ids, b_attn_mask)
-#       # Compute loss and accumulate the loss values
-#       loss = loss_fn(logits, b_labels)
-#       batch_loss += loss.item()
-#       total_loss += loss.item()
-#       # Perform a backward pass to calculate gradients
-#       loss.backward()
-#       # Clip the norm of the gradients to 1.0 to prevent "exploding gradients"
-#       torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-#       # Update parameters and the learning rate
-#       optimizer.step()
-#       scheduler.step()
-#       # Print the loss values and time elapsed for every 20 batches
-#       if (step % 20 == 0 and step != 0) or (step == len(train_dataloader) - 1):
-#         # Calculate time elapsed for 20 batches
-#         time_elapsed = time.time() - t0_batch
-#         # Print training results
-#         print(f"{epoch_i + 1:^7} | {step:^7} | {batch_loss / batch_counts:^12.6f} | {'-':^10} | {'-':^9} | {time_elapsed:^9.2f}")
-#         # Reset batch tracking variables
-#         batch_loss, batch_counts = 0, 0
-#         t0_batch = time.time()
-#         # Calculate the average loss over the entire training data
-#         avg_train_loss = total_loss / len(train_dataloader)
-#         print("-"*70)
-#
-#     # model_save_path =SAVE_PATH
-#     # model_to_save = bert_classifier.module if hasattr(bert_classifier,
-#     #                                                   'module') else bert_classifier  # Take care of distributed/parallel training
-#     # model_to_save.save_pretrained(model_save_path)
-#     # tokenizer.save_pretrained(model_save_path)
-#   # =======================================
-#   #               Evaluation
-#   # =======================================
-#     if evaluation == True:
-#       # After the completion of each training epoch, measure the model's performance
-#       # on our validation set.
-#       val_loss, val_accuracy = evaluate(model, val_dataloader)
-#       # Print performance over the entire training data
-#       time_elapsed = time.time() - t0_epoch
-#       print(f"{epoch_i + 1:^7} | {'-':^7} | {avg_train_loss:^12.6f} | {val_loss:^10.6f} | {val_accuracy:^9.2f} | {time_elapsed:^9.2f}")
-#       print("-"*70)
-#       print("\n")
-#       print("Training complete!")
-
 def evaluate(model, val_dataloader):
     """After the completion of each training epoch, measure the model's performance
     on our validation set.
@@ -397,35 +331,6 @@ def evaluate(model, val_dataloader):
 
     return val_loss, val_accuracy
 
-# def evaluate(model, val_dataloader):
-#   """After the completion of each training epoch, measure the model's performance
-#       on our validation set.
-#       """
-#   # Put the model into the evaluation mode. The dropout layers are disabled during
-#   # the test time.
-#   model.eval()
-#   # Tracking variables
-#   val_accuracy = []
-#   val_loss = []
-#   # For each batch in our validation set...
-#   for batch in val_dataloader:
-#     # Load batch to GPU
-#     b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
-#     # Compute logits
-#     with torch.no_grad():
-#       logits = model(b_input_ids, b_attn_mask)
-#       # Compute loss
-#       loss = loss_fn(logits, b_labels)
-#       val_loss.append(loss.item())
-#       # Get the predictions
-#       preds = torch.argmax(logits, dim=1).flatten()
-#       # Calculate the accuracy rate
-#       accuracy = (preds == b_labels).cpu().numpy().mean() * 100
-#       val_accuracy.append(accuracy)
-#     # Compute the average accuracy and loss over the validation set.
-#   val_loss = np.mean(val_loss)
-#   val_accuracy = np.mean(val_accuracy)
-#   return val_loss, val_accuracy
 
 set_seed(42)    # Set seed for reproducibility
 bert_classifier, optimizer, scheduler = initialize_model(epochs=EPOCH)
