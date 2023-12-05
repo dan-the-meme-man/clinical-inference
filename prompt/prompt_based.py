f767f2405379e7491d9c7203dc0dbefe8231b352
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-2.local>
2023-12-05 10:55:28 -0500

    make statement explicit in prompt

--- prompt/prompt_based.py
+++ prompt/prompt_based.py
@@ -3,14 +3,27 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import random
 from sklearn.metrics import classification_report
 from tqdm import tqdm
+import re
 '''
 You can experiment with:
 different gpt models: EleutherAI/gpt-j-6B
 different prompts: please change the lines related to prompt in the generate_prompt function
 few_shot learning parameters
 '''
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+
+def preprocess_data(text_list):
+    processed_text =''
+    for text in text_list:
+        if text.split()[0].isupper():
+            continue
+        pattern = r'[\u0000-\u001f\u007f-\u009f]'
+        # Replace the matched characters with an empty string
+        cleaned_text = re.sub(pattern, '', text)
+        processed_text+=cleaned_text
+    return processed_text
+
 
 def generate_prompt(training_data, dev_data, few_shot=2):
   prompt_length =[]
@@ -22,7 +35,7 @@ def generate_prompt(training_data, dev_data, few_shot=2):
   train_single = [x for x in train if x['secondary_text']==None]
   train_comp = [x for x in train if x['secondary_text']]
   for i, dt in enumerate(dev):
-    dt_primary = ' '.join([x.strip() for x in dt['primary_text']])
+    dt_primary = preprocess_data(dt['primary_text'])
     dt_label = dt['label'].strip()
     dt_statement = dt['statement'].strip()
     prompt_text =''
@@ -32,23 +45,23 @@ def generate_prompt(training_data, dev_data, few_shot=2):
       example_data = train_comp
     prompt_examples = random.sample(example_data, few_shot)
     for p in prompt_examples:
-      primary_text = ' '.join([x.strip() for x in p['primary_text']])
+      primary_text = preprocess_data(p['primary_text'])
 
       statement = p['statement'].strip()
       label = p['label'].strip()
 
       if p['secondary_text']:
-        secondary_text = ' '.join([x.strip() for x in p['secondary_text']])
-        prompt = f'Text1:{primary_text}\tText2:{secondary_text}\tStatement:{statement}\tLabel:{label}'
+        secondary_text = preprocess_data(p['primary_text'])
+        prompt = f'Statement:{statement}\tText1:{primary_text}\tText2:{secondary_text}\tLabel:{label}'
       else:
-        prompt = f'Text:{primary_text}\tStatement:{statement}\tLabel:{label}'
+        prompt = f'Statement:{statement}\tText:{primary_text}\tLabel:{label}'
 
       prompt_text+=prompt
     if dt['secondary_text']:
-      dt_secondary = ' '.join([x.strip() for x in dt['secondary_text']])
-      test_prompt = f'Text1:{dt_primary}\tText2:{dt_secondary}\tStatement:{dt_statement}\tLabel:'
+      dt_secondary = preprocess_data(dt['secondary_text'])
+      test_prompt = f'Statement:{dt_statement}\t Text1:{dt_primary}\tText2:{dt_secondary}\tLabel:'
     else:
-      test_prompt = f'Text:{dt_primary}\tStatement:{dt_statement}\tLabel:'
+      test_prompt = f'Statement:{dt_statement}\tText:{dt_primary}\tLabel:'
 
     prompt+=test_prompt
     prompts.append(prompt)
